aaf1d28e792fee8cb5718580e02c03d2636e6c1a
vladirim <vladimir0lomako@gmail.com>
2022-01-19 18:19:36 +0300

    feat: solve a task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+function reverse (n) {
+    let str = String(n);
+    let result = '';
+    for (let i = 0; i < str.length; i +=1) {
+         result = `${str[i]}${result}`;
+    }
+    
+    return parseInt(String(result));
 }
+
+module.exports = reverse;
+
+console.log(reverse(-124))
